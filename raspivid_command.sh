0224e2aee3762c69c413b1ab1d8b1dfb91e63e08
saaeg657 <ssaeg657@gmail.com>
2017-10-07 07:52:01 +0000

    add batch file

---
+++ raspivid_command.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+raspivid -o - -t 0 -vf -hf -n -fps 60 -b 6000000 -w 640 -h 480 | ffmpeg -thread_queue_size 8192 -re -ar 44100 -ac 2 -acodec pcm_s16le -f alsa -ac 2 -i plughw:1,0 -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://13.124.244.253/live
